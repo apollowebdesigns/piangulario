c7805592481467946a5e2f2b9ba5a3e22b6086af
Andy Evans <andrewevans0@outlook.com>
2018-01-22 19:35:08 +0000

    added routing to the app and new weather component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { WeathertableComponent } from './weathertable/weathertable.component';
+
+const appRoutes: Routes = [
+  { path: 'weathertable', component: WeathertableComponent },
+  { path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeathertableComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/weathertable/weathertable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weathertable',
+  templateUrl: './weathertable.component.html',
+  styleUrls: ['./weathertable.component.scss']
+})
+export class WeathertableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
