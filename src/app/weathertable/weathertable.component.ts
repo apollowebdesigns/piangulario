9c316c608b228023b48c39b2c8ec1890e313c991
Andy Evans <andrewevans0@outlook.com>
2018-01-22 21:07:16 +0000

    added observables

--- src/app/weathertable/weathertable.component.ts
+++ src/app/weathertable/weathertable.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-weathertable',
   templateUrl: './weathertable.component.html',
   styleUrls: ['./weathertable.component.scss']
 })
+
 export class WeathertableComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {
-    
+  getHeroes (): Observable<Object> {
+    return this.http.get(this.url)
   }
 
+  ngOnInit() {
+    this.getData();
+  }
+
+  model = 'hello';
   url = 'http://192.168.1.73:8080/demo/all';
 
   getData = () => {
@@ -20,8 +28,10 @@ export class WeathertableComponent implements OnInit {
       return response.json();
     }).then(function(data) {
       console.log(data);
+      this.model = JSON.stringify(data);
       return JSON.stringify(data);
-    }).catch(function() {
+    }).catch((error) => {
+      console.log(error);
       console.log("Booo");
     });
   }
