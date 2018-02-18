0490497c6da5309dd50810f743795ed25739c0e5
Andy Evans <andrewevans0@outlook.com>
2018-02-18 15:42:00 +0000

    added more to pi

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import * as EventSource from 'eventsource';
 
 @Component({
   selector: 'app-home',
@@ -8,16 +9,26 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
 
+  // vars init
+  sse; 
+  sse1;
+
   constructor(private http: HttpClient) { }
 
   /**
    * Terminates server sent event and stops cart from moving
    */
   killRequest() {
-    return this.http.get( "/end_motor_source");
+    return this.http.get( "http://192.168.1.73/end_motor_source");
   }
 
-
+  moveForwards() {
+    this.sse = new EventSource('http://192.168.1.73/my_event_source');
+    this.sse.onmessage = function(message) {
+        console.log('A message has arrived!');
+        // $('#output').append('<li>'+message.data+'</li>');
+    }
+  }
 
   ngOnInit() {
   }
